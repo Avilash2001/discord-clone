ce5ebb8dfcdfe5a7a1e72051bd29a4269e6b0253
Avilash2001 <avilashghosh2001@gmail.com>
2021-02-09 17:53:36 +0530

    bug fixes

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -33,12 +33,14 @@ function Chat() {
 
   const sendMessage = (e) => {
     e.preventDefault();
-    db.collection("channels").doc(channelId).collection("messages").add({
-      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-      message: input,
-      user: user,
-    });
-    setInput("");
+    if (input) {
+      db.collection("channels").doc(channelId).collection("messages").add({
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        message: input,
+        user: user,
+      });
+      setInput("");
+    }
   };
 
   return (
